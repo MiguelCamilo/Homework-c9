f0862e9433591017a19b11dd102e2490766332fc
Miguel C <52728963+MiguelCamilo@users.noreply.github.com>
2023-01-11 23:12:57 -0500

    homework for day 3, week 1 completed

---
+++ week1-homework/homework-day1.js
@@ -0,0 +1,12 @@
+// Date 01/09/22
+
+// log values of country & continent
+const country = 'USA';
+const continent = 'North America';
+
+console.log(country,continent);
+
+// create a variable and a assign a string
+const favoriteFood = 'Lasagna';
+
+const sum = '15' === 15; // will return false
---
+++ week1-homework/homework-day3.js
@@ -0,0 +1,61 @@
+// Date 01/11/23
+
+// 1. Using your loop of choice. Create a function that prints the pyramid below. (Show your personality by using your favorite emoji)
+function cloudPyramid() {
+    let space = '';
+    for (let i = 0; i < 5; i++) {
+        space += '❤️'
+        console.log(space)
+    }
+}
+cloudPyramid();
+
+// 2. Create a function that removes the first element of the array below and adds “kiwi” to the end of the array
+let favoriteFruits = ["mango","lychee", "rambutan","papaya"]
+
+function fruits(fruitArr) {
+
+    const removeFirst = fruitArr.shift() // removes first element in an arr
+    fruitArr.push('kiwi')
+    return removeFirst
+
+}
+const result = fruits(favoriteFruits)
+
+console.log(result)
+console.log(favoriteFruits)
+
+/* BONUS
+ Write a function named assignGrade, that takes one argument, a number score.
+ Return a grade for the score, either “A,” “B,” “C,” “D,” or “F.” Invoke that function for a few different scores and log the result to make sure it works.
+*/
+
+const letterGrade = ["A", "B", "C", "D", "F"]; // storing letter grades in an arr
+
+function assignGrade(score) {
+
+    if (score >= 90 && score <= 100 ) {
+
+        console.log(`Grade: ${letterGrade[0]}`)
+
+    } else if (score >= 80 && score <= 89) {
+
+        console.log(`Grade: ${letterGrade[1]}`)
+
+    } else if (score >= 70 && score <= 79) {
+
+        console.log(`Grade: ${letterGrade[2]}`)
+
+    } else if (score >= 60 && score <= 69) {
+
+        console.log(`Grade: ${letterGrade[3]}`)
+
+    } else if (score >= 50 && score <= 59) {
+
+        console.log(`Grade: ${letterGrade[4]}`)
+
+    }else {
+        console.log('You Failed!')
+    }
+}
+assignGrade(75);
