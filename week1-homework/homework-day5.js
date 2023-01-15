1eaf85c6f3f29865dee3b372aca897f6239729a6
Miguel Camilo <miguelcamilo48@gmail.com>
2023-01-15 15:42:10 -0500

    home day 5

---
+++ week1-homework/homework-day5.js
@@ -0,0 +1,24 @@
+// Create a function which returns the number of true values there are in an array.
+
+const testArray = [
+	true, // 
+	false,
+	false,
+	true, //
+	false,
+	false,
+	false,
+	true, //
+	true, //
+	true, //
+	false,
+];
+
+const trueInArr = (arr) => {
+    let counter = 0
+
+    // run through array and check each value
+    for (let i = 0; i < arr.length; i++ ) {
+        
+    }
+}
